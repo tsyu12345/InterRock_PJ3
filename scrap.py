8e3bfcb8b82537d36839e3b975b7ea31c254f19d
syu_asus_Note <syukunt@icloud.com>
2021-06-07 13:20:32 +0900

    3rd commit : ScrapInfoクラスの設計開始 ・URＬ取得時に、取得情報がjavaScript:void(0)になっており、ＵＲＬ が取得できていない。←Beautifulsoupの抽出タグの選択でどうにかなる。 ・許可年月の年号の部分が元サイトでは「R02」となっているのに対し、Excelに書き込むほうは西暦で記載されている。ので、変換する機能が必要

--- scrap.py
+++ scrap.py
@@ -1,8 +1,10 @@
 import openpyxl as px 
 import requests as rq
+from requests import exceptions as RqExceptions
 import threading 
 import re 
 import time
+import sys
 from selenium import webdriver
 import selenium
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
@@ -12,9 +14,19 @@ from bs4 import BeautifulSoup as bs
 class ScrapUrl:
     def __init__(self):
         self.options = webdriver.ChromeOptions()
-        self.options.add_argument('--headless')
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('--disable-gpu')
+        self.options.add_argument("start-maximized")
+        self.options.add_argument("enable-automation")
+        self.options.add_argument("--headless")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-infobars")
+        self.options.add_argument('--disable-extensions')
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--disable-browser-side-navigation")
+        self.options.add_argument("--disable-gpu")
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--ignore-ssl-errors')
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        self.options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 
     def search(self, area):
@@ -54,19 +66,81 @@ class ScrapUrl:
             time.sleep(2)
         return url_list
 
-"""
+
 class ScrapInfo:
-    def __init__(self):
-        pass
     
-    def scrap(self):
-"""
+    def scrap(self, url):
+        respons = rq.get(url)
+        print(url, end = "")
+        print(respons.status_code)
+        try:
+            #Scraiping Process here
+            soup = bs(respons, 'lxml')
+
+        except RqExceptions.RequestException as Error:
+            print("Exception Occured")
+            sys.exit(1)
+    
+    def call_jis_code(self, key):
+        pref_jiscode = {
+            "北海道": '01',
+            "青森県": '02',
+            "岩手県": '03',
+            "宮城県": '04',
+            "秋田県": '05',
+            "山形県": '06',
+            "福島県": '07',
+            "茨城県": '08',
+            "栃木県": '09',
+            "群馬県": 10,
+            "埼玉県": 11,
+            "千葉県": 12,
+            "東京都": 13,
+            "神奈川県": 14,
+            "新潟県": 15,
+            "富山県": 16,
+            "石川県": 17,
+            "福井県": 18,
+            "山梨県": 19,
+            "長野県": 20,
+            "岐阜県": 21,
+            "静岡県": 22,
+            "愛知県": 23,
+            "三重県": 24,
+            "滋賀県": 25,
+            "京都府": 26,
+            "大阪府": 27,
+            "兵庫県": 28,
+            "奈良県": 29,
+            "和歌山県": 30,
+            "鳥取県": 31,
+            "島根県": 32,
+            "岡山県": 33,
+            "広島県": 34,
+            "山口県": 35,
+            "徳島県": 36,
+            "香川県": 37,
+            "愛媛県": 38,
+            "高知県": 39,
+            "福岡県": 40,
+            "佐賀県": 41,
+            "長崎県": 42,
+            "熊本県": 43,
+            "大分県": 44,
+            "宮崎県": 45,
+            "鹿児島県": 46,
+            "沖縄県": 47
+        }
+        code = pref_jiscode[key]
+        print(code)
+        return code
+
+
+
 
 class WriteExcel:
-    
     book = px.Workbook()
     sheet = book.worksheets[0]
-
     def __init__(self):
         col_list = [
             "許可年月", 
@@ -113,7 +187,25 @@ class WriteExcel:
             "清掃施設工事業",
             "許可の有効期間",
         ]
+        for c in range(1, len(col_list)):
+            self.sheet.cell(row=1, column=c, value=col_list[c])
 
+    def write_data(self, index, col, write_data):
+        self.sheet.cell(row=index, column=col, value=write_data)
     
+    def save_book(self, save_path):
+        self.book.save(save_path)
+
+
+def main(save_path, area):
+    excel = WriteExcel()
+    scrap_url = ScrapUrl()
+    scrap_url.search(area)
+    url_list = scrap_url.scrap_url()
+    print(url_list)
+
+if __name__ == "__main__":
+    main("./test.xlsx", "01 北海道")
+
 
 
