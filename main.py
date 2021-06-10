732e6b0f1ab08d1a058979bf683a7ec2b3378a58
syu <syukunt@icloud.com>
2021-06-10 12:53:29 +0900

    guiの設計開始。エリアオブジェクトと保存先を指定するパスオブジェクトの作成

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 import PySimpleGUI as gui
-import sys
+import scrap
 
 
+class AreaSelect:
+    def lay_out(self):
+        L = [
+                [gui.Text("都道府県",
+                        key='pref_title', size=(60, None))],
+                [gui.InputText(key=('pref_name')), gui.Button('エリア選択')],
+            ]
+
+
+class PathSelect:
+
+
+def main():
+    gui.theme('BluePurple')
+    width = 700
+    height = 300
+    """
+    layout Object here
+    """
+    win = gui.Window('国土交通省 建設業許可 抽出ツール',
+                     icon='69b54a27564218141a41104e1e345cff_xxo.ico')
+    while True:
+        event, value = win.read()
+        # when window close
+        if event in ("Quit", None):
+            break
+    win.close()
+
+
+main()
