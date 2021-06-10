6f1b5fb9d67d7d6528a9940156559ba27e0d4578
syu <syukunt@icloud.com>
2021-06-10 15:06:29 +0900

    大まかなオブジェクトは完成、あとはscrapとの関連付け、チェック機能の実装、値をvalueに登録する。

--- main.py
+++ main.py
@@ -9,10 +9,56 @@ class AreaSelect:
                         key='pref_title', size=(60, None))],
                 [gui.InputText(key=('pref_name')), gui.Button('エリア選択')],
             ]
+        return L
 
+    def are_select(self):
+        prefs = '北海道,青森県,岩手県,宮城県,秋田県,山形県,福島県,茨城県,栃木県,群馬県,埼玉県,千葉県,東京都,神奈川県,新潟県,富山県,石川県,福井県,山梨県,長野県,岐阜県,静岡県,愛知県,三重県,滋賀県,京都府,大阪府,兵庫県,奈良県,和歌山県,鳥取県,島根県,岡山県,広島県,山口県,徳島県,香川県,愛媛県,高知県,福岡県,佐賀県,長崎県,熊本県,大分県,宮崎県,鹿児島県,沖縄県'
+        list_pref = prefs.split(',')
+        L = []
+        cnt = 0
+        for i in range(8):
+            add = []
+            for j in range(6):
+                if cnt != 47:
+                    add.append(gui.Checkbox(list_pref[cnt], key=list_pref[cnt]))
+                    cnt += 1
+            L.append(add)
+        L.append([gui.Button('OK', key='OK')])
+        window = gui.Window('エリア選択', layout=L)
+        pref = []
+        while True:
+            event, value = window.read()
+            print(event)
+            print(value)
+            for v in value.keys():
+                if value[v] == True:
+                    pref.append(v)
+            print(pref)
+            if event in ("Quit", None, 'OK'):
+                break
+        window.close()
+        return pref
+
+    
 
 class PathSelect:
 
+    def lay_out(self):
+        L = [
+            [gui.Text("フォルダ選択", key='path_title', size=(60, None))],
+            [gui.InputText(key='path'), gui.SaveAs("選択", file_types=( [('Excelファイル','*.xlsx')]))]
+        ]
+        return L
+
+
+
+def obj_frame(lay_out_data):
+    L = [
+            [gui.Frame("抽出条件", lay_out_data[0])],
+            [gui.Frame("保存先", lay_out_data[1])],
+            [gui.Button("抽出実行")]
+        ]
+    return L
 
 def main():
     gui.theme('BluePurple')
@@ -21,10 +67,18 @@ def main():
     """
     layout Object here
     """
+    area_obj = AreaSelect()
+    path_obj = PathSelect()
+    lay_data = [area_obj.lay_out(), path_obj.lay_out()]
+    layout = obj_frame(lay_data)
     win = gui.Window('国土交通省 建設業許可 抽出ツール',
-                     icon='69b54a27564218141a41104e1e345cff_xxo.ico')
+                     icon='69b54a27564218141a41104e1e345cff_xxo.ico', layout=layout)
     while True:
         event, value = win.read()
+        print(event)
+        print(value)
+        if event in 'エリア選択':
+            area_obj.are_select()
         # when window close
         if event in ("Quit", None):
             break
